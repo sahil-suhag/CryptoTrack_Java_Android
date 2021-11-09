c47da3122af07aafb1da812c8c13d5bd3bce55a4
Lovleen Bhalla <bhalla@debian>
2021-11-09 20:00:47 +0530

    Portfolio Data

--- app/src/main/java/com/bytehack/cryptotrack/Data.java
+++ app/src/main/java/com/bytehack/cryptotrack/Data.java
@@ -1,6 +1,38 @@
 package com.bytehack.cryptotrack;
 
+
+
 public class Data {
+    /**
+     * JSON scheme for portfolio data
+     * [
+     *   {
+     *     "name": "DogeCoin",
+     *     "id": "dgcoin1",
+     *     "quantity": 10.00000
+     *   },
+     *   {
+     *     "name": "Bitcoin",
+     *     "id": "btc1",
+     *     "quantity": 0.0000001
+     *   },
+     *   {
+     *     "name": "Ethereum",
+     *     "id": "ethereun1",
+     *     "quantity": 10.00000
+     *   },
+     *   {
+     *     "name": "Ripple",
+     *     "id": "riplecoin1",
+     *     "quantity": 9.999999
+     *   },
+     *   {
+     *     "name": "Litecoin",
+     *     "id": "litecoint1",
+     *     "quantity": 5.555555
+     *   }
+     * ]
+     */
     // Dummy data of the trader
-    public static final String WALLET_DATA = "[\n  {\n    \"name\": \"DogeCoin\",\n    \"id\": \"dgcoin1\",\n    \"quantity\": 10.00000\n  },\n  {\n    \"name\": \"Bitcoin\",\n    \"id\": \"btc1\",\n    \"quantity\": 0.0000001\n  },\n  {\n    \"name\": \"Ethereum\",\n    \"id\": \"ethereun1\",\n    \"quantity\": 10.00000\n  },\n  {\n    \"name\": \"Ripple\",\n    \"id\": \"riplecoin1\",\n    \"quantity\": 9.999999\n  },\n  {\n    \"name\": \"Litecoin\",\n    \"id\": \"litecoint1\",\n    \"quantity\": 5.555555\n  }\n]";
+    public static final String PORTFOLIO_DATA = "[\n  {\n    \"name\": \"DogeCoin\",\n    \"id\": \"dgcoin1\",\n    \"quantity\": 10.00000\n  },\n  {\n    \"name\": \"Bitcoin\",\n    \"id\": \"btc1\",\n    \"quantity\": 0.0000001\n  },\n  {\n    \"name\": \"Ethereum\",\n    \"id\": \"ethereun1\",\n    \"quantity\": 10.00000\n  },\n  {\n    \"name\": \"Ripple\",\n    \"id\": \"riplecoin1\",\n    \"quantity\": 9.999999\n  },\n  {\n    \"name\": \"Litecoin\",\n    \"id\": \"litecoint1\",\n    \"quantity\": 5.555555\n  }\n]";
 }
